8d5af0c8b8cd1fef5f6ca8836d401edee6ca6b3b
Kiselev-Dmitry <kdv150275@gmail.com>
2023-11-11 17:55:50 +0300

    Combine script in parse_book_page function

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ from urllib.parse import urljoin
 from urllib.parse import urlparse
 
 
+def parse_book_page(url, id):
+    response = requests.get("{}b{}/".format(url, id))
+    response.raise_for_status()
+    if not response.history:
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        title, author = soup.find('h1').text.split("::")
+        title = title.strip()
+        author = author.strip()
+        print("Заголовок: ", title)
+        print("Автор: ", author)
+
+        genres_elements = soup.find("span", class_="d_book").find_all("a")
+        genres = []
+        for element in genres_elements:
+            genres.append(element.text)
+        print("Жанр: ", genres)
+
+        image = soup.find(class_='bookimage').find('img')['src']
+        image_url = urljoin(url, image)
+        print("Ссылка на картинку: ", image_url) # отладочный код
+
+        return title, image_url
+
 def download_txt(url, id, folder='books/'):
     """Функция для скачивания текстовых файлов.
     Args:
@@ -22,17 +46,7 @@ def download_txt(url, id, folder='books/'):
         file_name = '{}. {}.txt'.format(id, sanitize_filename(get_title(url, id)))
 #        save_file(response, file_name, folder)
 #        download_image(url, id)
-        get_genres(url, id)
-
-
-def get_title(url, id):
-    response = requests.get("{}b{}/".format(url, id))
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    title, author = soup.find('h1').text.split("::")
-    title = title.strip()
-    print("Заголовок: ", title)
-    return title
+#        get_genres(url, id)
 
 
 def download_image(url, id):
@@ -48,17 +62,6 @@ def download_image(url, id):
 #    save_file(response, file_name, "images")
 
 
-def get_genres(url, id):
-    response = requests.get("{}b{}/".format(url, id))
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    genres_elements = soup.find("span", class_="d_book").find_all("a")
-    genres = []
-    for element in genres_elements:
-        genres.append(element.text)
-    print(genres)
-
-
 def save_file(response, file_name, folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -67,8 +70,16 @@ def save_file(response, file_name, folder):
         file.write(response.content)
 
 
-if __name__ == '__main__':
+def main():
     url = "https://tululu.org/"
     folder = 'books/'
     for index in range(1, 11):
-        download_txt(url, str(index), folder)
+        parse_book_page(url, str(index))
+
+#        download_txt(url, str(index), folder)
+
+
+if __name__ == '__main__':
+    main()
+
+
