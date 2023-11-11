c399f2d93ff630b014a348bdfb6af91ed949910e
Kiselev-Dmitry <kdv150275@gmail.com>
2023-11-11 15:04:31 +0300

    download book covers

--- main.py
+++ main.py
@@ -2,19 +2,8 @@ import requests
 import os
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
-
-
-def get_title(url, id):
-#    url = "{}b{}/".format(url, id)
-    response = requests.get("{}b{}/".format(url, id))
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    title, author = soup.find('h1').text.split("::")
-    title = title.strip()
-#    author = author.strip()
-#    print("Заголовок: ", title)
-#    print("Автор: ", author)
-    return title
+from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 
 def download_txt(url, id, folder='books/'):
@@ -26,18 +15,44 @@ def download_txt(url, id, folder='books/'):
     Returns:
         str: Путь до файла, куда сохранён текст.
     """
-#    url = "{}txt.php".format(main_url)
     payload = {"id": id}
     response = requests.get("{}txt.php".format(url), params=payload)
     response.raise_for_status()
     if not response.history:
-        filename = '{}. {}.txt'.format(id, sanitize_filename(get_title(url, id)))
-        file_path = os.path.join(folder, filename)
-#        print(file_path)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
+        file_name = '{}. {}.txt'.format(id, sanitize_filename(get_title(url, id)))
+        save_file(response, file_name, folder)
+        download_image(url, id)
+
+
+def get_title(url, id):
+    response = requests.get("{}b{}/".format(url, id))
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title, author = soup.find('h1').text.split("::")
+    title = title.strip()
+    print("Заголовок: ", title)
+    return title
+
+
+def download_image(url, id):
+    response = requests.get("{}b{}/".format(url, id))
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    image = soup.find(class_='bookimage').find('img')['src']
+    image_url = urljoin(url, image)
+    print("Ссылка на картинку: ", image_url)
+    dir_name, file_name = os.path.split(image)
+    response = requests.get(image_url)
+    response.raise_for_status()
+    save_file(response, file_name, "images")
+
+
+def save_file(response, file_name, folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    file_path = os.path.join(folder, file_name)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
 
 if __name__ == '__main__':
