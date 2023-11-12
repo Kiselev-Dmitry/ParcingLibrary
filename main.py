1ae3f3312523c28912478c526d904eda4b7fa0c8
Kiselev-Dmitry <kdv150275@gmail.com>
2023-11-12 14:19:12 +0300

    Correct refferences between all functions

--- main.py
+++ main.py
@@ -26,9 +26,11 @@ def parse_book_page(url, id):
 
         image = soup.find(class_='bookimage').find('img')['src']
         image_url = urljoin(url, image)
+        image_dir, image_name = os.path.split(image)
         print("Ссылка на картинку: ", image_url) # отладочный код
 
-        return title, image_url
+        return title, image_name, image_url
+
 
 def download_txt(url, id, folder='books/'):
     """Функция для скачивания текстовых файлов.
@@ -43,23 +45,17 @@ def download_txt(url, id, folder='books/'):
     response = requests.get("{}txt.php".format(url), params=payload)
     response.raise_for_status()
     if not response.history:
-        file_name = '{}. {}.txt'.format(id, sanitize_filename(get_title(url, id)))
-#        save_file(response, file_name, folder)
-#        download_image(url, id)
-#        get_genres(url, id)
+        title, image_name, image_url = parse_book_page(url, str(id))
+        file_name = '{}. {}.txt'.format(id, sanitize_filename(title))
+        save_file(response, file_name, folder)
+        download_image(image_name, image_url, id)
 
 
-def download_image(url, id):
-    response = requests.get("{}b{}/".format(url, id))
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    image = soup.find(class_='bookimage').find('img')['src']
-    image_url = urljoin(url, image)
-    print("Ссылка на картинку: ", image_url)
-    dir_name, file_name = os.path.split(image)
+def download_image(image_name, image_url, id):
+    image_folder = "images"
     response = requests.get(image_url)
     response.raise_for_status()
-#    save_file(response, file_name, "images")
+    save_file(response, image_name, image_folder)
 
 
 def save_file(response, file_name, folder):
@@ -70,16 +66,15 @@ def save_file(response, file_name, folder):
         file.write(response.content)
 
 
-def main():
-    url = "https://tululu.org/"
-    folder = 'books/'
-    for index in range(1, 11):
-        parse_book_page(url, str(index))
-
+# def main():
+#     url = "https://tululu.org/"
+#     folder = 'books/'
+#     for index in range(1, 11):
 #        download_txt(url, str(index), folder)
 
 
 if __name__ == '__main__':
-    main()
-
-
+    url = "https://tululu.org/"
+    folder = 'books/'
+    for index in range(1, 11):
+       download_txt(url, str(index), folder)
