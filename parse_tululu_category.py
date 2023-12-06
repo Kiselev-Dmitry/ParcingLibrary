00df32889bb50202e4e4f7cedcdcf14951d424dc
Kiselev-Dmitry <kdv150275@gmail.com>
2023-12-06 21:46:41 +0300

    Improve try-except

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -113,32 +113,40 @@ def main():
                 soup = BeautifulSoup(response.text, 'lxml')
                 book_tags = soup.select(".d_book")
                 for book_tag in book_tags:
-                    book_href = book_tag.select_one("a")["href"]
-                    book_url = urljoin("https://tululu.org", book_href)
+                    try:
+                        book_href = book_tag.select_one("a")["href"]
+                        book_url = urljoin("https://tululu.org", book_href)
 
-                    response = requests.get(book_url)
-                    response.raise_for_status()
-                    check_for_redirect(response)
-                    book_params = parse_book_page(response)
+                        response = requests.get(book_url)
+                        response.raise_for_status()
+                        check_for_redirect(response)
+                        book_params = parse_book_page(response)
 
-                    if not args.skip_txt:
-                        book_path = download_txt(book_url, index, book_params["title"], books_folder)
-                    else:
-                        book_path = None
-                    if not args.skip_imgs:
-                        image_path = download_image(book_params["image_name"], book_params["image_url"], image_folder)
-                    else:
-                        image_path = None
+                        if not args.skip_txt:
+                            book_path = download_txt(book_url, index, book_params["title"], books_folder)
+                        else:
+                            book_path = None
+                        if not args.skip_imgs:
+                            image_path = download_image(book_params["image_name"], book_params["image_url"], image_folder)
+                        else:
+                            image_path = None
 
-                    books.append({
-                        "title": book_params["title"],
-                        "author": book_params["author"],
-                        "book_path": book_path,
-                        "image_path": image_path,
-                        "genres": book_params["genres"],
-                        "comments": book_params["comments"]
-                    })
-                    index += 1
+                        books.append({
+                            "title": book_params["title"],
+                            "author": book_params["author"],
+                            "book_path": book_path,
+                            "image_path": image_path,
+                            "genres": book_params["genres"],
+                            "comments": book_params["comments"]
+                        })
+                        index += 1
+
+                    except requests.HTTPError as error:
+                        logging.warning(error)
+                        break
+                    except requests.ConnectionError as error:
+                        logging.error(error)
+                        time.sleep(5)
 
             except requests.HTTPError as error:
                 logging.warning(error)
